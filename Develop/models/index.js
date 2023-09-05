112cd32fdacde5c3e502eeb74187ba9f336a23d7
CJ <cjmendenhall24@hotmail.com>
2023-09-05 16:11:13 -0400

    updated gitignore to not ignore env

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -15,12 +15,14 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
   foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
-  foreignKey: 'product_id',
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
